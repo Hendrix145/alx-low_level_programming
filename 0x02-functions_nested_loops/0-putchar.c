e448e3072259a41940c5f45cb7204c067ce9b8c0
HENDRIX145 <eniadeadeniran@gmail.com>
2022-03-16 14:13:08 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - writes the character _putchar to stdout
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and error is set appropriately.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
