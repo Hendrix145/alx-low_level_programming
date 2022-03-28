dfc1c20f290d87837d79fa40114f314aad589df1
HENDRIX145 <eniadeadeniran@gmail.com>
2022-03-28 13:04:33 -0700

    my first commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer we need to set to
+ * @to: string to set
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+© 2022 GitHub, Inc.
+Terms
